4ac23f5c6ac98cef9f92b73153298dc7e245e148
marioterrazas <marioeduardoterrazasparada@gmail.com>
2024-05-02 08:58:27 -0400

    Pryecto final Dwb2-UPDS

---
+++ app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component'; // Importa HeroesComponent aquí
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HeroesComponent // Aquí se importa HeroesComponent como parte de los imports
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
